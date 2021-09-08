1d439fe8653374de15b17f0198e66383999ce0fe
abhishekgaikwad10 <abhishekgaikwad326@gmail.com>
2021-09-08 11:16:19 +0530

    welcom

---
+++ src/com/abhi/empwage/Main.java
@@ -0,0 +1,9 @@
+package com.abhi.empwage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to empwage");
+	// write your code here
+    }
+}
