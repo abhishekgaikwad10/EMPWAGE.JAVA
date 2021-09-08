ba6183199730ceacf272f3316f0079ccb5504344
abhishekgaikwad10 <abhishekgaikwad326@gmail.com>
2021-09-08 11:24:19 +0530

    emp prestnt or absent

--- src/com/abhi/empwage/Main.java
+++ src/com/abhi/empwage/Main.java
@@ -3,7 +3,18 @@ package com.abhi.empwage;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("welcome to empwage");
-	// write your code here
+        //constant
+        int is_full_time = 1;
+
+        //computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == 1) {
+            System.out.println("EMPLOYEE IS PRESENT");
+        } else {
+            System.out.println("EMPLOYEE IS ABSENT");
+        }
     }
 }
+
+
+
